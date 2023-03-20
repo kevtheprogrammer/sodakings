5081b7ca00b9139a3e608ccfd26b953bd7a180e6
Kelvin Banda <72869357+shazziedev@users.noreply.github.com>
2023-03-20 15:08:40 +0200

    first commit

---
+++ acc/models.py
@@ -0,0 +1,71 @@
+from __future__ import unicode_literals
+from django.db import models
+from django.core.mail import send_mail
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.base_user import AbstractBaseUser
+
+# from django.urls import reverse
+
+# from django.db.models import CheckConstraint, Q, UniqueConstraint
+# from django.core.validators import MinValueValidator, MaxValueValidator
+
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+
+from .managers import UserManager
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # username 
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    email = models.EmailField(verbose_name='email address', unique=True)
+    # email_confirmed = models.BooleanField(verbose_name='Email Confirmed', default=False)
+    first_name = models.CharField(
+        verbose_name='first name', max_length=30, blank=True)
+    last_name = models.CharField(verbose_name='last name', max_length=30, blank=True)
+    dob = models.DateField(auto_now=True, blank=True)
+    nrc = models.CharField(verbose_name='national registration number', max_length=300, blank=True)
+    country = CountryField(blank_label='(select country)', blank=True)
+    phone =  PhoneNumberField(blank=True)
+    is_staff = models.BooleanField(verbose_name='staff', default=False)
+    location = models.CharField(verbose_name='location', max_length=50, blank=True)
+    date_joined = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
+    is_active = models.BooleanField(verbose_name='active', default=True)
+    is_verified = models.BooleanField(verbose_name='verified', default=False)
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        
+    
+    def get_full_name(self):
+        '''
+        Returns the first_name plus the last_name, with a space in between.
+        '''
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        '''
+        Returns the short name for the user.
+        '''
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        print(subject, message, from_email,)
+        # send_mail(subject, message, from_email, [self.email], **kwargs)
+        
+
+    def email_farmer_registration(self, subject, message, from_email=None, **kwargs):
+        '''
+        Sends an email to this User.
+        '''
+        print('\n',subject, '\n', message,'\n', from_email,'\n')
+        # send_mail(subject, message, from_email, [self.email], **kwargs)
+        
---
+++ acc/views.py
@@ -0,0 +1,81 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+# from django.shortcuts import render,redirect
+# from django.views.generic import View
+# from django.contrib.sites.shortcuts import get_current_site
+# from django.template.loader import render_to_string
+# from django.utils.encoding import force_bytes
+# from django.utils.http import urlsafe_base64_encode
+# from django.contrib import messages
+# from django.utils.encoding import force_str
+# from django.utils.http import urlsafe_base64_decode
+# from django.contrib.auth import login , logout
+
+
+
+# from .tokens import email_activation_token
+# from .models import User
+# from .userForms import SignUpForm
+# from acc.userForms import UserEditForm
+# from farmer.models import FarmPlotModel
+
+
+# class SignUpView(View):
+#     form_class = SignUpForm
+#     template_name = 'registration/signup.html'
+
+#     def get(self, request, *args, **kwargs):
+#         ctx = {
+#             'form': self.form_class() ,
+#         }
+#         return render(request, self.template_name, ctx)
+
+#     def post(self, request, *args, **kwargs):
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+
+#             user = form.save(commit=False)
+#             user.is_active = False #u need to deactivate account till it is confirmed
+#             user.save()
+#             FarmPlotModel.objects.create(name="default",size="1 x 1(acres)",farm_location="default",user=user)
+#             #send activation email
+
+#             this_site = get_current_site(request)
+#             site = this_site.domain
+#             subject = 'Activate Your %(site) Account'
+#             message = render_to_string('registration/account_activation_email.html', {
+#                 'user': user,
+#                 'domain': site,
+#                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+#                 'token': email_activation_token.make_token(user),
+#             })
+#             user.email_user(subject, message)
+
+#             messages.success(request, ('We have sent a confirmation link to your email to activate your account\nPlease follow your email and click on the link!'))
+
+#             return redirect('account:login')
+
+#         return render(request, self.template_name, {'form': form})
+
+
+# class ActivateAccount(View):
+
+#     def get(self, request, uidb64, token, *args, **kwargs):
+#         try:
+#             uid = force_str(urlsafe_base64_decode(uidb64))
+#             user = User.objects.get(pk=uid)
+#         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+#             user = None
+
+#         if user is not None and email_activation_token.check_token(user, token):
+#             user.is_active = True
+            
+#             user.save()
+#             login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+#             messages.success(request, ('Your email has been confirmed.'))
+#             return redirect('home')
+#         else:
+#             messages.warning(request, ('The confirmation link was invalid, possibly because it has already been used.'))
+#             return redirect('home')
+
